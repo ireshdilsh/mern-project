f8cee6fb75e0b568ed1e2a78baab77a3554169b9
ireshdilsh <ireshdilshan46@gmail.com>
2025-09-29 14:59:50 +0530

    refactor Landing component for improved readability and structure

--- mern-front/src/pages/Landing.tsx
+++ mern-front/src/pages/Landing.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Landing() {
     return (
-        <div className='flex flex-col justify-center items-center h-screen bg-gray-100 gap-24'>
-            <div className=''>
+        <div id='main-page' className='min-h-screen bg-gray-100'>
+            <div id="hero-section" className='flex flex-col items-start justify-left pt-12 pb-12 pl-36 pr-36'>
                 <h1 className='text-5xl font-bold mb-4 leading-16'>Weave your thoughts,<br /> frame your story.</h1>
                 <p className='text-base text-gray-500 font-medium'>Loom & Lens features in-depth writing and personal essays that explore ideas, <br /> culture, and creativity. Join a community of curious minds.</p>
                 <div className='mt-5 flex gap-2'>
@@ -11,7 +11,6 @@ export default function Landing() {
                     <button className='bg-gray-300 text-gray-700 px-4 py-2 rounded cursor-pointer font-medium text-base'>Join Now</button>
                 </div>
             </div>
-
         </div>
     )
 }
