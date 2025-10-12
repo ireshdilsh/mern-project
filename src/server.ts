c447ce793ce8fb87368352dfce205fd284f7aa22
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-12 12:17:47 +0530

    feat: enhance server setup with CORS and JSON parsing middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import cors from "cors";
 
 const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(express.urlencoded({extended:true}));
+
+// add for routes
+
 
 server.listen(5000,()=>{
     console.log("Server is starting")
