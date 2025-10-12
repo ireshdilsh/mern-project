48bcd046b4fcd6b1bcbe71812ae9f84b635e3087
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-12 14:51:20 +0530

    Refactor Dashboard component: update search input background color and add notification message; add Notification page component

--- mern-front/src/pages/Dashboard.tsx
+++ mern-front/src/pages/Dashboard.tsx
@@ -10,7 +10,7 @@ export default function Dashboard() {
       <nav className='pl-40 pr-40 pt-4 pb-4 border-b border-gray-200 w-full flex justify-between items-center'>
         <div className='flex justify-center items-center gap-8'>
           <img src={logo} alt="" className='h-11 cursor-pointer' onClick={() => { navigate('/') }} />
-          <input type="text" className='bg-gray-100 outline-none h-11 w-56 rounded-4xl pl-6 pr-6' placeholder='search'/>
+          <input type="text" className='bg-neutral-100 outline-none h-11 w-56 rounded-4xl pl-6 pr-6' placeholder='search' />
         </div>
         <div className='flex gap-7 items-center justify-center'>
           <button onClick={() => { navigate('/write') }} className='flex items-center gap-2 text-gray-500 cursor-pointer text-sm'><img src="https://img.icons8.com/?size=100&id=NNvh71bQhaH2&format=png&color=99a1af" alt="" className='h-5.5' />Write</button>
@@ -19,7 +19,10 @@ export default function Dashboard() {
         </div>
       </nav>
 
-
+      <div className='bg-neutral-100 p-6 rounded-sm mt-5 mb-5 ml-80 mr-80'>
+        <p className='text-sm font-medium'>We’ve made some improvements to how you keep track of your favorite content and creators.
+          Your “Following” list and saved topics are now combined into the new Following Page, giving you a single place to explore all the updates you care about. </p>
+      </div>
 
     </div>
   )
