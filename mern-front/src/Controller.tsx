75563f694ce36e71f0d0f06c2bb5b03b58a0826e
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-15 20:25:20 +0530

    Refactor Controller and Landing components: improve structure and readability

--- mern-front/src/Controller.tsx
+++ mern-front/src/Controller.tsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
 import Landing from './pages/Landing'
-import Help from './pages/Help'
-import TermsService from './pages/TermsService'
-import About from './pages/About'
-import PrivacyPolicy from './pages/PrivacyPolicy'
-import Write from './pages/Write'
-import Notification from './pages/Notification'
-import GetArticleByID from './pages/GetArticleByID'
+
 
 export default function Controller() {
   return (
@@ -16,14 +9,6 @@ export default function Controller() {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element = {<Landing/>}></Route>
-                <Route path='/help-center' element= {<Help/>}></Route>'
-                <Route path='/dashboard' element = {<Dashboard/>}></Route>
-                <Route path='/terms-service' element = {<TermsService/>}></Route>
-                <Route path='/privacy-policy' element = {<PrivacyPolicy/>}></Route>
-                <Route path='/about-us' element = {<About/>}></Route>
-                <Route path='/write' element = {<Write/>}></Route>
-                <Route path='/notification' element = {<Notification/>}></Route>
-                <Route path='/fetch/article/by/id=/:id/and/title/:title' element = {<GetArticleByID/>}></Route>
             </Routes>
         </BrowserRouter>
     </div>
