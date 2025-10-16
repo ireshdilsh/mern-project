9ef65e6cfb2dfbaf17ec16f4195b6a9332d1feb1
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-16 11:49:19 +0530

    Add Login page and integrate navigation from Register page

--- mern-front/src/Controller.tsx
+++ mern-front/src/Controller.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Register from './pages/Register'
+import Login from './pages/Login'
 
 
 export default function Controller() {
@@ -11,6 +12,7 @@ export default function Controller() {
             <Routes>
                 <Route path='/' element = {<Landing/>}></Route>
                 <Route path='/create/account' element = {<Register/>}></Route>
+                <Route path='/login/user/account' element = {<Login/>}></Route>
             </Routes>
         </BrowserRouter>
     </div>
