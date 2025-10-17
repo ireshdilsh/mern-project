cfc589c55760982f1359a7edc3cf1f97493f4a22
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-17 21:51:03 +0530

    Refactor Navbar component styles and update search icon color for better visibility

--- mern-front/src/component/Navbar.tsx
+++ mern-front/src/component/Navbar.tsx
@@ -5,19 +5,20 @@ import '../styles/navbar.css'
 export default function Navbar() {
     return (
         <div className='flex justify-center items-center w-full' id='navbar-page'>
-            <nav className='flex justify-between items-center w-full px-30'>
-                <div className='flex justify-center items-center gap-15'>
+            <nav className='flex justify-between items-center w-full px-30 border-b border-b-neutral-100 py-1'>
+                <div className='flex justify-center items-center gap-12'>
                     <img src={logo} alt="navbar-logo" className='h-16' />
                     <div className='flex justify-center items-center relative'>
-                        <img src="https://img.icons8.com/?size=100&id=59878&format=png&color=000000" alt="search-icon" className='h-6 absolute right-6 cursor-pointer' />
-                        <input type="text" placeholder='Search here ...' className='bg-neutral-50 h-11 w-85 rounded-4xl px-5 outline-none' />
+                        <img src="https://img.icons8.com/?size=100&id=59878&format=png&color=a3a3a3" alt="search-icon" className='h-6 absolute right-6 cursor-pointer' />
+                        <input type="text" placeholder='Search here ...' className='bg-neutral-50 h-11 w-85 rounded-4xl px-5 outline-none pr-15' />
                     </div>
                 </div>
-               <div className='flex justify-center items-center gap-15'>
-                    <div className='flex justify-center items-center gap-5'>
-                        <img src="https://img.icons8.com/?size=100&id=36871&format=png&color=000000" alt="" />
-                        <p>Write</p>
+               <div className='flex justify-center items-center gap-8'>
+                    <div className='flex justify-center items-center gap-2 cursor-pointer'>
+                        <img src="https://img.icons8.com/?size=100&id=36929&format=png&color=a3a3a3" alt="" className='h-6'/>
+                        <p className='text-neutral-500 text-sm'>Write</p>
                     </div>
+                        <img src="https://img.icons8.com/?size=100&id=62atSgaif9UE&format=png&color=a3a3a3" alt="" className='h-5 cursor-pointer'/>
                </div>
             </nav>
         </div>
