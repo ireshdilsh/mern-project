8b2ee09acff37b7b37d236a0dcd4f4518482e058
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-17 21:56:52 +0530

    Refactor Navbar component for improved layout and styling consistency

--- mern-front/src/component/Navbar.tsx
+++ mern-front/src/component/Navbar.tsx
@@ -6,11 +6,14 @@ export default function Navbar() {
     return (
         <div className='flex justify-center items-center w-full' id='navbar-page'>
             <nav className='flex justify-between items-center w-full px-30 border-b border-b-neutral-100 py-1'>
+                <div className='flex justify-center items-center gap-8'>
+                    <img src="https://img.icons8.com/?size=100&id=36389&format=png&color=000000" alt="menu-icon" className='h-5 cursor-pointer' />
                     <img src={logo} alt="navbar-logo" className='h-16' />
-                    <div className='flex justify-center items-center relative'>
-                        <img src="https://img.icons8.com/?size=100&id=59878&format=png&color=a3a3a3" alt="search-icon" className='h-6 absolute right-6 cursor-pointer' />
-                        <input type="text" placeholder='Search here ...' className='bg-neutral-50 h-11 w-85 rounded-4xl px-5 outline-none pr-15' />
-                    </div>
+                </div>
+                <div className='flex justify-center items-center relative'>
+                    <img src="https://img.icons8.com/?size=100&id=59878&format=png&color=a3a3a3" alt="search-icon" className='h-6 absolute right-6 cursor-pointer' />
+                    <input type="text" placeholder='Search here ...' className='bg-neutral-50 h-11 w-85 rounded-4xl px-5 outline-none pr-15' />
+                </div>
             </nav>
         </div>
     )
