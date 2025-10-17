b716f23cd6853cb76672beec9e9d8f159a009c61
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-17 22:56:16 +0530

    Remove background color from Dashboard component layout

--- mern-front/src/pages/Dashboard.tsx
+++ mern-front/src/pages/Dashboard.tsx
@@ -5,7 +5,7 @@ export default function Dashboard() {
   return (
     <div className='flex justify-center items-center flex-col w-full h-full' id='dashboard-page'>
       <Navbar />
-        <div className='absolute left-25 top-14 bg-amber-300 h-full w-355'>
+        <div className='absolute left-25 top-14 h-full w-355'>
           
         </div>
     </div>
