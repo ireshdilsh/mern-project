993de0bf035a45977d486f4a55438c8cf1d22cb0
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-17 22:03:39 +0530

    Add off-canvas menu functionality to Navbar component

--- mern-front/src/component/Navbar.tsx
+++ mern-front/src/component/Navbar.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.png'
 import '../styles/navbar.css'
 
 export default function Navbar() {
+
+    const [offCanvas, setOffCanvas] = useState<boolean>(false);
+
+    const openCanvas = (): void => {
+        setOffCanvas(true)
+    }
+
     return (
         <div className='flex justify-center items-center w-full' id='navbar-page'>
             <nav className='flex justify-between items-center w-full px-30 border-b border-b-neutral-100 py-1'>
                 <div className='flex justify-center items-center gap-8'>
-                    <img src="https://img.icons8.com/?size=100&id=36389&format=png&color=000000" alt="menu-icon" className='h-5 cursor-pointer' />
+                    <img src="https://img.icons8.com/?size=100&id=36389&format=png&color=000000" onClick={openCanvas} alt="menu-icon" className='h-5 cursor-pointer' />
                     <img src={logo} alt="navbar-logo" className='h-16' />
                 </div>
                 <div className='flex justify-center items-center relative'>
@@ -15,6 +22,8 @@ export default function Navbar() {
                     <input type="text" placeholder='Search here ...' className='bg-neutral-50 h-11 w-85 rounded-4xl px-5 outline-none pr-15' />
                 </div>
             </nav>
+
+
         </div>
     )
 }
