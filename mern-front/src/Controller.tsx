8180f8d249d53ebdb3c8d577bab42db2922ebc96
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-17 20:06:23 +0530

    Add Dashboard page and integrate routing in Controller

--- mern-front/src/Controller.tsx
+++ mern-front/src/Controller.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 
 export default function Controller() {
@@ -13,6 +14,7 @@ export default function Controller() {
                 <Route path='/' element = {<Landing/>}></Route>
                 <Route path='/create/account' element = {<Register/>}></Route>
                 <Route path='/login/user/account' element = {<Login/>}></Route>
+                <Route path='/dashboard' element = {<Dashboard/>}></Route>
             </Routes>
         </BrowserRouter>
     </div>
