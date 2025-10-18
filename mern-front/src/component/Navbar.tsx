60fb2e7daf129301cb399f091957b61647787f5f
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-18 10:14:01 +0530

    Refactor Navbar component for improved layout and usability

--- mern-front/src/component/Navbar.tsx
+++ mern-front/src/component/Navbar.tsx
@@ -7,6 +7,14 @@ export default function Navbar() {
 
     const navigate : NavigateFunction = useNavigate()
 
+    const gotoWritePage = ():void => {
+        navigate('/write/page/')
+    }
+
+    const gotoNotificationsPage = ():void => {
+        navigate('/notification/page')
+    }
+
     return (
         <div className='flex justify-center items-center flex-col w-full' id='navbar-page'>
             <nav className='flex justify-between items-center w-full px-50 border-b border-b-neutral-100 py-1'>
@@ -18,13 +26,13 @@ export default function Navbar() {
                     </div>
                 </div>
                 <div className='flex justify-center items-center gap-8'>
-                    <div className='flex justify-center items-center gap-2 cursor-pointer'>
+                    <div onClick={gotoWritePage} className='flex justify-center items-center gap-2 cursor-pointer'>
                         <img src="https://img.icons8.com/?size=100&id=kZ0eCZEs0tfC&format=png&color=000000" alt="" className='h-5'/>
                         <p className='text-sm'>Write</p>
                     </div>
-                    <img src="https://img.icons8.com/?size=100&id=62atSgaif9UE&format=png&color=000000" className='h-5' alt="" />
+                    <img onClick={gotoNotificationsPage} src="https://img.icons8.com/?size=100&id=62atSgaif9UE&format=png&color=000000" className='h-5 cursor-pointer' alt="" />
                     <div className='flex justify-center items-center rounded-full h-12 w-12 bg-neutral-300 cursor-pointer'>
-                        <p>ID</p>
+                        <p className='text-base font-medium'>ID</p>
                     </div>
                 </div>
             </nav>
