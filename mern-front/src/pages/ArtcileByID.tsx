7f82a27cd36a9fd07b755ac51fc49f23f7976b87
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-19 15:27:52 +0530

    Fix typo in ArticleByID component name and improve formatting; add Navbar component

--- mern-front/src/pages/ArtcileByID.tsx
+++ mern-front/src/pages/ArtcileByID.tsx
@@ -2,31 +2,35 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import type { Article } from '../types/Article';
+import Navbar from '../component/Navbar';
 
 export default function ArtcileByID() {
 
-    const {id} = useParams<{id: string}>()
-    const [article,setArticle] = useState<Article>()
+    const { id } = useParams<{ id: string }>()
+    const [article, setArticle] = useState<Article>()
 
     useEffect(() => {
-        const featchArticle = async() => {
+        const featchArticle = async () => {
             if (!id) {
                 console.log("No article ID provided");
                 return;
             }
-            
+
             try {
                 const resp = await axios.get(`http://localhost:5000/api/v1/article/get/article/${id}`)
                 setArticle(resp.data.article);
                 console.log(resp.data.article);
             } catch (error) {
-                console.log("Something went wrong" ,error);
+                console.log("Something went wrong", error);
             }
         }
         featchArticle()
     }, [id]);
 
-  return (
-    <div>ArtcileByID = {article?._id}</div>
-  )
+    return (
+        <div className='flex justify-center items-center flex-col'>
+            <Navbar/>
+            ArtcileByID = {article?._id}
+        </div>
+    )
 }
