4a360c63d1c170c8962559b187e32f1b7cf9d13a
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-21 14:55:06 +0530

    Fix typo in fetchArticle function name in ArticleByID component

--- mern-front/src/pages/ArtcileByID.tsx
+++ mern-front/src/pages/ArtcileByID.tsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
-import {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
-import type {Article} from '../types/Article';
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import type { Article } from '../types/Article';
 import Navbar from '../component/Navbar';
 import '../styles/articlebyid.css'
 
 export default function ArtcileByID() {
 
-    const {id} = useParams<{ id: string }>()
+    const { id } = useParams<{ id: string }>()
     const [article, setArticle] = useState<Article>()
 
     useEffect(() => {
@@ -40,9 +40,10 @@ export default function ArtcileByID() {
         featchArticle()
     }
 
+
     return (
         <div className='flex justify-center items-center w-full flex-col'>
-            <Navbar/>
+            <Navbar />
             <div className='flex justify-center items-start flex-col mt-20 w-195' id='article-by-id-page'>
                 <div className='inline-block bg-green-100 px-3 rounded-4xl'>
                     <p className='text-sm whitespace-nowrap text-green-900'>{article?.title} by {article?.name}</p>
@@ -55,19 +56,19 @@ export default function ArtcileByID() {
                 <div className='flex justify-center items-center gap-8 mt-5'>
                     <div className='flex justify-center items-center gap-3'>
                         <img src="https://img.icons8.com/?size=100&id=85028&format=png&color=333333" className='h-5'
-                             alt=""/>
+                            alt="" />
                         <p>{article?.viewCount}</p>
                     </div>
                     <div className='flex justify-center items-center gap-3 cursor-pointer' onClick={incrementLikes}>
                         <img
                             src="https://img.icons8.com/?size=100&id=33481&format=png&color=333333" className='h-5'
-                            alt=""/>
+                            alt="" />
                         <p>{article?.likeCount}</p>
                     </div>
-                    
+
                     <div className='flex justify-center items-center gap-3 cursor-pointer' onClick={incrementDisLikes}>
                         <img src="https://img.icons8.com/?size=100&id=15957&format=png&color=333333" className='h-5'
-                             alt=""/>
+                            alt="" />
                         <p>{article?.disLikeCount}</p>
                     </div>
                 </div>
@@ -77,7 +78,7 @@ export default function ArtcileByID() {
                         sentence.trim() && (
                             <span key={index}>
                                 {sentence.trim()}.
-                                {index < article.content.split('.').length - 1 && <><br/><br/></>}
+                                {index < article.content.split('.').length - 1 && <><br /><br /></>}
                             </span>
                         )
                     ))}
