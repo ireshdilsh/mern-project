e4d03b24b0f402447cc5148176416b4953a01e5b
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-22 13:40:14 +0530

    refactor: remove comments from comment routes for cleaner code

--- src/routes/commentRoutes.ts
+++ src/routes/commentRoutes.ts
@@ -8,16 +8,9 @@ import {
 
 const router = express.Router();
 
-// Create a new comment
 router.post("/add/comment", createComment);
-
-// Get all comments for a specific article
 router.get("/get/comments/article/:articleId", getCommentsByArticleId);
-
-// Get single comment by ID
 router.get("/get/comment/:id", getCommentById);
-
-// Get comment count for an article
 router.get("/get/comment/count/:articleId", getCommentCount);
 
 export default router;
