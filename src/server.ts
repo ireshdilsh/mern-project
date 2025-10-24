7baea389f3840c88a70f01348f45c8e9e5155cb1
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-24 12:44:26 +0530

    refactor: update server startup message for clarity

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,26 @@
 import express, {type Express, urlencoded} from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./config/dbConfig.js";
+import {connectDB} from "./config/dbConfig.js";
 import userRoutes from "./routes/userRoutes.js";
 import articleRoutes from "./routes/articleRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
+
 dotenv.config();
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
-server.use(express.urlencoded({extended:true}));
+server.use(express.urlencoded({extended: true}));
 
 // add for routes
-server.use('/api/v1/user',userRoutes)
-server.use('/api/v1/article',articleRoutes)
-server.use('/api/v1/comment',commentRoutes)
+server.use('/api/v1/user', userRoutes)
+server.use('/api/v1/article', articleRoutes)
+server.use('/api/v1/comment', commentRoutes)
 
-server.listen(5000,()=>{
+server.listen(5000, () => {
     console.log("Server is starting port 5000");
 })
 
- connectDB();
+connectDB();
