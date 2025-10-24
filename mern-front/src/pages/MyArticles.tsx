4783893766d6593a4bd1e991eda6f56c14894a00
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-24 13:06:59 +0530

    Add MyArticles component with basic layout

---
+++ mern-front/src/pages/MyArticles.tsx
@@ -0,0 +1,7 @@
+export default function MyArticles() {
+    return (
+        <div className='flex justify-center items-center flex-col'>
+            <h1 className='text-3xl'>Hello World</h1>
+        </div>
+    )
+}
