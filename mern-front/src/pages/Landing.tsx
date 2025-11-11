020190881980adb03da11b89f193a110033fb5f4
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-11 12:28:26 +0530

    Add hero image to Landing component and update image source

--- mern-front/src/pages/Landing.tsx
+++ mern-front/src/pages/Landing.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import heroimg from '../assets/hero-img.png'
 export default function Landing() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
@@ -82,7 +82,7 @@ export default function Landing() {
             <button className='bg-blue-500 text-white text-lg px-5 py-2 rounded-4xl cursor-pointer'>Learn more</button>
             <button className='border border-blue-500 text-blue-500 rounded-4xl px-5 py-2 text-lg cursor-pointer hover:bg-blue-500 hover:text-white'>Buy</button>
           </div>
-          <img src="" alt="" />
+          <img src={heroimg} alt="iPhone 17 Pro" className='h-120 scale-100 -mt-15'/>
      </section>
 
       {/* Footer */}
