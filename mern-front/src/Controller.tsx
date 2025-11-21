e0654c7172c2fbbfeb945d057cc8f5b39ef23e56
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-21 12:24:29 +0530

    chore: update routing structure for improved navigation

--- mern-front/src/Controller.tsx
+++ mern-front/src/Controller.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from './pages/Landing'
+import SignIn from './pages/auth/SignIn'
+import SignUp from './pages/auth/SignUp'
 
 export default function Controller() {
   return (
@@ -7,6 +9,8 @@ export default function Controller() {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Landing />} />
+                <Route path='/login' element={<SignIn />} />
+                <Route path='/register' element={<SignUp />} />
             </Routes>
         </BrowserRouter>
     </div>
