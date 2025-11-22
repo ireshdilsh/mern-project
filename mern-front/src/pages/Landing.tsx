30c4ed77921e4a3e458bf0e5a574282f32b977f0
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-22 14:44:37 +0530

    feat: implement mobile menu toggle functionality in Landing component

--- mern-front/src/pages/Landing.tsx
+++ mern-front/src/pages/Landing.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 export default function Landing() {
+
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+
   return (
-    <div className='flex justify-center items-center flex-col'>
-        <nav className='flex justify-center items-center'>
-          
-        </nav>
+    <div className='flex justify-center items-center flex-col w-full'>
+       
     </div>
   )
 }
