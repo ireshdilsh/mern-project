74777e27ab19c2dad7cefcc7a7fe91fc116c78fa
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-22 16:43:50 +0530

    feat: add BlogSection, ContentSection, and TeamSection components with dynamic content

--- mern-front/src/pages/Landing.tsx
+++ mern-front/src/pages/Landing.tsx
@@ -1,6 +1,10 @@
+import AboutSection from "../sections/AboutSection";
+import BlogSection from "../sections/BlogSection";
+import ContentSection from "../sections/ContentSection";
 import FeaturesSection from "../sections/FeaturesSection";
 import HeroSection from "../sections/HeroSection";
 import PricingSection from "../sections/PricingSection";
+import TeamSection from "../sections/TeamSection";
 
 export default function Landing() {
 
@@ -9,6 +13,10 @@ export default function Landing() {
       <HeroSection/>
       <FeaturesSection/>
       <PricingSection/>
+      <AboutSection/>
+      <TeamSection/>
+      <BlogSection/>
+      <ContentSection/>
     </div>
   )
 }
