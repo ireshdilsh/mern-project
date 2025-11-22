84f9b80d725a99a9889beb6cfbb479605cdd4c86
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-22 15:07:44 +0530

    feat: update FeaturesSection and HeroSection styles for improved readability and design consistency

--- mern-front/src/sections/HeroSection.tsx
+++ mern-front/src/sections/HeroSection.tsx
@@ -15,7 +15,7 @@ export default function HeroSection() {
 
     return (
         <div>
-            <div className="bg-white dark:bg-gray-900">
+            <div className="bg-white">
                 <header className="absolute inset-x-0 top-0 z-50">
                     <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
                         <div className="flex lg:flex-1">
@@ -24,12 +24,7 @@ export default function HeroSection() {
                                 <img
                                     alt=""
                                     src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                                    className="h-8 w-auto dark:hidden"
-                                />
-                                <img
-                                    alt=""
-                                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                                    className="h-8 w-auto not-dark:hidden"
+                                    className="h-8 w-auto"
                                 />
                             </a>
                         </div>
@@ -37,7 +32,7 @@ export default function HeroSection() {
                             <button
                                 type="button"
                                 onClick={() => setMobileMenuOpen(true)}
-                                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-gray-200"
+                                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
                             >
                                 <span className="sr-only">Open main menu</span>
                                 <Bars3Icon aria-hidden="true" className="size-6" />
@@ -45,51 +40,46 @@ export default function HeroSection() {
                         </div>
                         <div className="hidden lg:flex lg:gap-x-12">
                             {navigation.map((item) => (
-                                <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-900 dark:text-white">
+                                <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-900">
                                     {item.name}
                                 </a>
                             ))}
                         </div>
                         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                            <a href="#" className="text-sm/6 font-semibold text-gray-900 dark:text-white">
+                            <a href="#" className="text-sm/6 font-semibold text-gray-900">
                                 Log in <span aria-hidden="true">&rarr;</span>
                             </a>
                         </div>
                     </nav>
                     <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
                         <div className="fixed inset-0 z-50" />
-                        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:bg-gray-900 dark:sm:ring-gray-100/10">
+                        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                             <div className="flex items-center justify-between">
                                 <a href="#" className="-m-1.5 p-1.5">
                                     <span className="sr-only">Your Company</span>
                                     <img
                                         alt=""
                                         src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                                        className="h-8 w-auto dark:hidden"
-                                    />
-                                    <img
-                                        alt=""
-                                        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                                        className="h-8 w-auto not-dark:hidden"
+                                        className="h-8 w-auto"
                                     />
                                 </a>
                                 <button
                                     type="button"
                                     onClick={() => setMobileMenuOpen(false)}
-                                    className="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-gray-200"
+                                    className="-m-2.5 rounded-md p-2.5 text-gray-700"
                                 >
                                     <span className="sr-only">Close menu</span>
                                     <XMarkIcon aria-hidden="true" className="size-6" />
                                 </button>
                             </div>
                             <div className="mt-6 flow-root">
-                                <div className="-my-6 divide-y divide-gray-500/10 dark:divide-white/10">
+                                <div className="-my-6 divide-y divide-gray-500/10">
                                     <div className="space-y-2 py-6">
                                         {navigation.map((item) => (
                                             <a
                                                 key={item.name}
                                                 href={item.href}
-                                                className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-white dark:hover:bg-white/5"
+                                                className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                                             >
                                                 {item.name}
                                             </a>
@@ -98,7 +88,7 @@ export default function HeroSection() {
                                     <div className="py-6">
                                         <a
                                             href="#"
-                                            className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-white dark:hover:bg-white/5"
+                                            className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                                         >
                                             Log in
                                         </a>
@@ -124,30 +114,30 @@ export default function HeroSection() {
                     </div>
                     <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
                         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                            <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:text-gray-400 dark:ring-white/10 dark:hover:ring-white/20">
+                            <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                                 Announcing our next round of funding.{' '}
-                                <a href="#" className="font-semibold text-indigo-600 dark:text-indigo-400">
+                                <a href="#" className="font-semibold text-indigo-600">
                                     <span aria-hidden="true" className="absolute inset-0" />
                                     Read more <span aria-hidden="true">&rarr;</span>
                                 </a>
                             </div>
                         </div>
                         <div className="text-center">
-                            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl dark:text-white">
+                            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
                                 Data to enrich your online business
                             </h1>
-                            <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">
+                            <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
                                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
                                 fugiat veniam occaecat.
                             </p>
                             <div className="mt-10 flex items-center justify-center gap-x-6">
                                 <a
                                     href="#"
-                                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
+                                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
                                     Get started
                                 </a>
-                                <a href="#" className="text-sm/6 font-semibold text-gray-900 dark:text-white">
+                                <a href="#" className="text-sm/6 font-semibold text-gray-900">
                                     Learn more <span aria-hidden="true">→</span>
                                 </a>
                             </div>
