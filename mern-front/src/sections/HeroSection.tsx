553f81fc01b9443d6629cb85c0ab6cd24e839998
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-23 23:35:24 +0530

    fix: enhance sign-in modal with improved styling and messaging

--- mern-front/src/sections/HeroSection.tsx
+++ mern-front/src/sections/HeroSection.tsx
@@ -25,7 +25,7 @@ export default function HeroSection() {
           <a href="">Pricing</a>
           <a href="">About</a>
           <a href="">Contact</a>
-          <button onClick={() => { setOpenSignInModal(true) }}>Sign in</button>
+          <p className = 'cursor-pointer' onClick={() => { setOpenSignInModal(true) }}>Sign in</p>
         </div>
       </nav>
       <div className='flex justify-center items-center flex-col pb-35 mt-29'>
@@ -43,8 +43,16 @@ export default function HeroSection() {
       {/* Sign in modal */}
       {openSignInModal && (
         <div className='w-full h-full absolute top-0 left-0 flex justify-center items-center' id='modal-overlay'>
-            <div className='bg-white py-15 px-20 w-100 flex justify-center items-center flex-col rounded-lg'>
-              <h1 className='text-3xl font-semibold'>Sign in</h1>
+            <div className='bg-white py-15 px-15 w-120 flex justify-center items-center flex-col rounded-lg'>
+              <img onClick={()=>{setOpenSignInModal(false)}} className='h-4 cursor-pointer -mr-100 -mt-6' src="https://img.icons8.com/?size=100&id=88571&format=png&color=000000" alt="close-icn" />
+              <h1 className='text-3xl font-semibold mt-5'>Good to See You Again !</h1>
+              <p className='text-neutral-500 text-center mt-3'>Enter your details to reconnect with your memories and tools instantly.</p>
+              <button className='cursor-pointer mt-5 border border-neutral-400 rounded-sm w-full py-2 font-bold text-neutral-700 flex justify-center items-center gap-5'><img className='h-6' src="https://img.icons8.com/?size=100&id=17949&format=png&color=000000" alt="" />Continue with google</button>
+              <div className='flex justify-center items-center gap-4 mt-3.5'>
+                <p className='text-neutral-700'>If you haven't an account ?</p>
+                <p className='text-blue-500 cursor-pointer'>Sign up</p>
+              </div>
+              <p className='text-neutral-500 mt-5 text-center' style={{fontSize:'12px'}}>By clicking "Sign in", you accept Medium's Terms of Service and Privacy Policy.</p>
             </div>
         </div>
       )}
