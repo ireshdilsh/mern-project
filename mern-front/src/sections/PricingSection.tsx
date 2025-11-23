1afa92da30f4a005ea88825cf0ec531e87039748
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-23 21:47:33 +0530

    fix: complete PricingSection component by adding missing plan details

--- mern-front/src/sections/PricingSection.tsx
+++ mern-front/src/sections/PricingSection.tsx
@@ -7,7 +7,7 @@ export default function PricingSection() {
         <p className='text-neutral-500 text-base mt-2'>Whether you're just starting out or scaling fast, our pricing is built to match your needs and budget.</p>
         <div className='flex justify-center items-center gap-5'>
             <div className='flex justify-start items-center flex-col border border-neutral-300 rounded-md p-10'>
-
+                <h1></h1>
             </div>
         </div>
     </div>
