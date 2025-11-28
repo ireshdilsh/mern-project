b4215ca4f9f8cb89eef3f788f595d0b63b69bfc0
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-28 22:28:09 +0530

    feat: add album creation route and integrate album routes into server

--- src/routes/AlbumRoutes.ts
+++ src/routes/AlbumRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import {createNewAlbum} from "../controller/AlbumController.js";
 
 const router = express.Router();
 
 // Define album-related routes here
-router.post()
+router.post('/create/new/album',createNewAlbum)
+
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, {type Express, urlencoded} from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import {connectDB} from "./config/dbConfig.js";
+import router from "./routes/AlbumRoutes.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ server.use(cors());
 server.use(express.urlencoded({extended: true}));
 
 // custom routes paths
+server.use('/api/v1/albums',router)
 
 server.listen(5000, () => {
     console.log("Server is starting port 5000");
