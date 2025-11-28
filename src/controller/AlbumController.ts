95f216c8162b23353acb87a4f35d8e89e93e293b
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-28 23:34:27 +0530

    feat: update album controller to use async/await and improve error handling

--- src/controller/AlbumController.ts
+++ src/controller/AlbumController.ts
@@ -1,25 +1,23 @@
 import {Album} from "../model/Album.js";
 
-export const createNewAlbum = (req:any,res:any) => {
-    const {title} = req.body;
-
-    const newAlbum = new Album({title});
-
-    newAlbum.save()
-    .then((album) => {
-        res.status(201).json(album);
-    })
-    .catch((error) => {
-        res.status(500).json({error: "Error creating album"});
-    });
+export const createNewAlbum = async (req: any, res: any) => {
+    try {
+        const {title} = req.body;
+        const newAlbum = new Album({title});
+        await newAlbum.save();
+        res.status(200).json({message: "Album created successfully"});
+    } catch (e) {
+        console.error(e);
+        res.status(400).json({message: "Something Went Wrong"});
+    }
 }
 
-export const getAllAlbums = (req:any,res:any) => {
-    Album.find()
-    .then((albums) => {
-        res.status(200).json(albums);
-    })
-    .catch((error) => {
-        res.status(500).json({error: "Error fetching albums"});
-    });
+export const getAllAlbums = async (req: any, res: any) => {
+    try {
+        const allAlbums = await Album.find();
+        res.status(200).json({message: "Success", albums: allAlbums});
+    } catch (e) {
+        console.error(e);
+        res.status(400).json({message: "Something Went Wrong"});
+    }
 }
--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ server.use(cors());
 server.use(express.urlencoded({extended: true}));
 
 // custom routes paths
-server.use('/api/v1/albums',router)
+server.use('/albums/api/v1/',router)
 
 server.listen(5000, () => {
     console.log("Server is starting port 5000");
