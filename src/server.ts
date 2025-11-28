0c6d5207cb8de092349bf4a7639f33e187c0be47
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-28 22:15:47 +0530

    refactor: update MongoDB URI and clean up server routes

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,6 @@ import express, {type Express, urlencoded} from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import {connectDB} from "./config/dbConfig.js";
-import userRoutes from "./routes/userRoutes.js";
-import articleRoutes from "./routes/articleRoutes.js";
-import commentRoutes from "./routes/commentRoutes.js";
 
 dotenv.config();
 
@@ -13,11 +10,6 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(express.urlencoded({extended: true}));
-
-// add for routes
-server.use('/api/v1/user', userRoutes)
-server.use('/api/v1/article', articleRoutes)
-server.use('/api/v1/comment', commentRoutes)
 
 server.listen(5000, () => {
     console.log("Server is starting port 5000");
