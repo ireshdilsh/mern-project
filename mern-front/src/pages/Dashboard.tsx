dc3340524f74b4c36d0cb1ade2f9f13e9f594f96
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-28 21:18:16 +0530

    feat: update Dashboard navigation with new menu items and user avatar

--- mern-front/src/pages/Dashboard.tsx
+++ mern-front/src/pages/Dashboard.tsx
@@ -6,7 +6,10 @@ export default function Dashboard() {
             <nav className='flex justify-between items-center w-full px-35 py-3 border-b border-b-neutral-200'>
                 <div className='logo cursor-pointer'>MemoraAI</div>
                 <div className='flex justify-center items-center gap-5'>
-                                                                                            
+                    <p className='cursor-pointer'>My Memories</p>
+                    <p className='cursor-pointer'>Create New</p>
+                    <p className='cursor-pointer'>Settings</p>
+                    <div className='w-8 h-8 rounded-full bg-neutral-400 flex justify-center items-center text-white cursor-pointer'>A</div>
                 </div>
             </nav>
         </div>
