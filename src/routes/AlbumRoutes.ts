1383b06eb6b2b730f5d699e1ba86f84f2e7218d2
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-28 22:30:13 +0530

    feat: add route to retrieve all albums

--- src/routes/AlbumRoutes.ts
+++ src/routes/AlbumRoutes.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import {createNewAlbum} from "../controller/AlbumController.js";
+import {createNewAlbum, getAllAlbums} from "../controller/AlbumController.js";
 
 const router = express.Router();
 
 // Define album-related routes here
 router.post('/create/new/album',createNewAlbum)
-
+router.get('/get/all/albums',getAllAlbums)
 
 export default router;
