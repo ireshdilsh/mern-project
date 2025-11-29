3744fea243903f2cfea66143a20090f8200bcb8a
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-29 13:50:26 +0530

    feat: update Landing page with new content and styling

--- mern-front/src/Controller.tsx
+++ mern-front/src/Controller.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Landing from './pages/Landing'
 
 export default function Controller() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          
+          <Route path='/' element={<Landing/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- mern-front/src/pages/Landing.tsx
+++ mern-front/src/pages/Landing.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Landing() {
   return (
     <div>
-        <h1>Welcome to the Landing Page</h1>
+        <h1 className='text-6xl font-semibold tracking-tighter'>Create Viral YouTube Thumbnails in Seconds</h1>
+        <p className='text-neutral-400 text-lg'>Turn ideas into high-impact thumbnails with AI. Generate, style, and optimize your thumbnails instantly—no design skills needed.</p>
     </div>
   )
 }
