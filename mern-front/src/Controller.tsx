f9cde1fbd6cbfa343f4d5beafbd81839212845ee
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-29 13:33:49 +0530

    refactor: remove unused page imports from Controller component

--- mern-front/src/Controller.tsx
+++ mern-front/src/Controller.tsx
@@ -1,12 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Landing from './pages/Landing'
-import About from './pages/About'
-import Features from './pages/Features'
-import Gallery from './pages/Gallery'
-import Pricing from './pages/Pricing'
-import HowWorks from './pages/HowWorks'
-import Dashboard from './pages/Dashboard'
-import AlbumWithID from './pages/AlbumWithID'
 
 export default function Controller() {
   return (
