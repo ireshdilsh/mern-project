53a9a9c76dfb5015d207edb8c81c91d78cc5e3ab
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-29 16:38:28 +0530

    feat: implement Navbar component with sign-in and sign-up modals

---
+++ mern-front/src/components/Navbar.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import logo from '../assets/logo.png'
+import login from '../assets/login.jpeg'
+import '../styles/landing.css'
+
+export default function Navbar() {
+
+    const [signInModal, setsignInModal] = useState(false);
+    const [signUpModal, setsignUpModal] = useState(false);
+
+    const openSininModal = () => {
+        setsignInModal(true)
+    }
+
+    const closeSigninModal = () => {
+        setsignInModal(false)
+    }
+
+    return (
+        <div>
+            <nav className='mt-5 flex justify-between items-center px-35'>
+                <img src={logo} alt="ai-logo" className='h-8' />
+                <div className="flex justify-center items-center gap-4">
+
+                </div>
+                <div className="flex justify-center items-center gap-2">
+                    <button onClick={openSininModal} className='font-medium cursor-pointer hover:bg-neutral-100 px-3.5 py-2 rounded-3xl'>Sign in</button>
+                    <button className='font-medium cursor-pointer bg-[#333] text-sm text-white px-3.5 py-2 rounded-3xl hover:opacity-80'>Sign up</button>
+                </div>
+            </nav>
+
+            {/* Sign in Modal */}
+            {signInModal && (
+                <div className='flex justify-center items-center flex-col w-full h-full absolute left-0 top-0' id='modal-overlay'>
+                    <div className='bg-white w-4xl rounded-md flex justify-between items-center'>
+                        <div className='flex flex-col p-10 w-115'>
+                            <img src={logo} className='w-30 mb-4' alt="" />
+                            <h1 className='text-2xl font-semibold text-[#333] tracking-tighter'>Sign In to Continue</h1>
+                            <p className='mt-3 text-neutral-500 w-96 text-justify text-sm'>Manage your projects, generate thumbnails, and keep your progress synced across devices.</p>
+                            <div className='mt-10 flex flex-col gap-2'>
+                                <button className='w-96 border border-neutral-200 py-2.5 rounded-3xl flex justify-center items-center gap-6 cursor-pointer font-medium'><img src="https://img.icons8.com/?size=100&id=85500&format=png&color=333333" alt="" className='h-5.5'/>Continue With Email</button>
+                                <button className='w-96 border border-neutral-200 py-2.5 rounded-3xl flex justify-center items-center gap-6 cursor-pointer font-medium'><img src="https://img.icons8.com/?size=100&id=V5cGWnc9R4xj&format=png&color=000000" alt="" className='h-5.5'/>Continue With Google</button>
+                            </div>
+                        </div>
+                        <div className='h-full'>
+                            <img id='signin-img' src={login} alt="" className='overflow-hidden h-full'/>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+
+            {/* Sign up Modal */}
+        </div>
+    )
+}
--- mern-front/src/pages/Landing.tsx
+++ mern-front/src/pages/Landing.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import logo from '../assets/logo.png'
+import Navbar from '../components/Navbar'
 
 export default function Landing() {
   return (
     <div>
-      <nav className='mt-5 flex justify-between items-center px-35'>
-        <img src={logo} alt="ai-logo" className='h-8' />
-        <div className="flex justify-center items-center gap-4">
-          
-        </div>
-        <div className="flex justify-center items-center gap-2">
-          <button className='font-medium cursor-pointer hover:bg-neutral-100 px-3.5 py-2 rounded-3xl'>Sign in</button>
-          <button className='font-medium cursor-pointer bg-[#333] text-sm text-white px-3.5 py-2 rounded-3xl hover:opacity-80'>Sign up</button>
-        </div>
-      </nav>
+     <Navbar/>
       <div className='flex flex-col justify-center items-center mt-35 px-35'>
         <h1 className='text-6xl font-semibold tracking-tighter text-[#333] text-center'>Create Viral <span className='bg-clip-text text-transparent bg-gradient-to-r to-blue-600 via-indigo-600 from-pink-600'>YouTube <br /> Thumbnails</span> in Seconds.</h1>
         <p className='text-neutral-400 text-lg mt-2'>Turn ideas into high-impact thumbnails with AI. Generate, style, and optimize your thumbnails instantly—no design skills needed.</p>
