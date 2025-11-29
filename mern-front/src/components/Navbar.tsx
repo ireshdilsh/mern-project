edb7d07a6746f7c206f962b68f421964b0b6d0ab
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-29 16:56:30 +0530

    feat: enhance Sign In modal with improved layout and additional options

--- mern-front/src/components/Navbar.tsx
+++ mern-front/src/components/Navbar.tsx
@@ -32,18 +32,24 @@ export default function Navbar() {
             {/* Sign in Modal */}
             {signInModal && (
                 <div className='flex justify-center items-center flex-col w-full h-full absolute left-0 top-0' id='modal-overlay'>
-                    <div className='bg-white w-4xl rounded-md flex justify-between items-center'>
-                        <div className='flex flex-col p-10 w-115'>
+                    <div className='bg-white  rounded-md flex justify-between items-center h-126 relative'>
+                        <img src="https://img.icons8.com/?size=100&id=83149&format=png&color=000000" className='absolute right-5 cursor-pointer top-5 h-5' alt="close-icn" onClick={closeSigninModal}/>
+                        <div className='flex flex-col p-10 '>
                             <img src={logo} className='w-30 mb-4' alt="" />
                             <h1 className='text-2xl font-semibold text-[#333] tracking-tighter'>Sign In to Continue</h1>
-                            <p className='mt-3 text-neutral-500 w-96 text-justify text-sm'>Manage your projects, generate thumbnails, and keep your progress synced across devices.</p>
+                            <p className='mt-3 text-neutral-400 w-96 text-justify text-sm'>Manage your projects, generate thumbnails, and keep your progress synced across devices.</p>
                             <div className='mt-10 flex flex-col gap-2'>
                                 <button className='w-96 border border-neutral-200 py-2.5 rounded-3xl flex justify-center items-center gap-6 cursor-pointer font-medium'><img src="https://img.icons8.com/?size=100&id=85500&format=png&color=333333" alt="" className='h-5.5'/>Continue With Email</button>
                                 <button className='w-96 border border-neutral-200 py-2.5 rounded-3xl flex justify-center items-center gap-6 cursor-pointer font-medium'><img src="https://img.icons8.com/?size=100&id=V5cGWnc9R4xj&format=png&color=000000" alt="" className='h-5.5'/>Continue With Google</button>
                             </div>
+                            <div className='mt-6 flex w-96 justify-between items-center'>
+                                <p>If you haven't an account ?</p>
+                                <button className='text-blue-600 text-sm cursor-pointer font-medium'>Signup here</button>
+                            </div>
+                            <p className='text-sm w-96 text-neutral-400 mt-8 text-center'>By continuing, you confirm that you have read and agree to our Terms and Privacy Policy.</p>
                         </div>
                         <div className='h-full'>
-                            <img id='signin-img' src={login} alt="" className='overflow-hidden h-full'/>
+                            <img id='signin-img' src={login} alt="" className='overflow-hidden w-130 h-full'/>
                         </div>
                     </div>
                 </div>
