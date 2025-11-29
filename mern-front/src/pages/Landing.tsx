1aed34b9f4efeb62253c4cf056385765848c077f
ireshdilsh <ireshdilshan46@gmail.com>
2025-11-29 17:30:13 +0530

    feat: update Landing page header and description for enhanced clarity and appeal

--- mern-front/src/pages/Landing.tsx
+++ mern-front/src/pages/Landing.tsx
@@ -6,8 +6,8 @@ export default function Landing() {
     <div>
      <Navbar/>
       <div className='flex flex-col justify-center items-center mt-35 px-35'>
-        <h1 className='text-6xl font-semibold tracking-tighter text-[#333] text-center'>Create Viral <span className='bg-clip-text text-transparent bg-gradient-to-r to-blue-600 via-indigo-600 from-pink-600'>YouTube <br /> Thumbnails</span> in Seconds.</h1>
-        <p className='text-neutral-400 text-lg mt-2'>Turn ideas into high-impact thumbnails with AI. Generate, style, and optimize your thumbnails instantly—no design skills needed.</p>
+        <h1 className='text-7xl font-semibold tracking-tighter text-[#333] text-center'>Create Stunning <span className='bg-clip-text text-transparent bg-gradient-to-r to-blue-600 via-indigo-600 from-pink-600'>AI Images</span> <br /> in Seconds.</h1>
+        <p className='text-neutral-400 text-lg mt-2'>Turn your ideas into vibrant, high-quality images with AI. Generate, style, and customize artwork instantly—no design skills required.</p>
         <div className='flex justify-center items-center gap-4 mt-7'>
           <input type="text" className='w-3xl h-12 border border-neutral-200 rounded-3xl px-5 outline-none' placeholder='Type your thumbnail idea…' />
           <button className='bg-[#333] rounded-3xl flex justify-center items-center px-5 py-2.5 cursor-pointer h-12'><img className='h-6' src="https://img.icons8.com/?size=100&id=60700&format=png&color=ffffff" alt="send-icon" /></button>
