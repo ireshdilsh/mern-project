e5390103d4aa37ecf355f4654d980de0819fbf66
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-03 13:55:07 +0530

    chore: clean up Landing component structure

--- mern-front/src/pages/Landing.tsx
+++ mern-front/src/pages/Landing.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Blocks from './Blocks'
+import logo from '../assets/logo.png'
 
 export default function Landing() {
   return (
-    <div>
+    <div className='w-full'>
       <Blocks/>
+
+      <div className='flex flex-col justify-center items-center absolute top-0 left-0 w-full px-35'>
+        <nav className='flex justify-between items-center w-full mt-1 bg-white py-5'>
+            <img src={logo} className='h-8' alt="" />
+          <div>
+            dfgdfg
+          </div>
+        </nav>
+      </div>
+
     </div>
   )
 }
