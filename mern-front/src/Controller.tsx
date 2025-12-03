18fb93e36e89083dea27de2a0c854564a4a52535
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-03 13:04:06 +0530

    chore: remove commented-out URLs from .env file

--- mern-front/src/Controller.tsx
+++ mern-front/src/Controller.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from './pages/Landing'
-import HowItWorks from './pages/HowItWorks'
 
 export default function Controller() {
   return (
@@ -8,7 +7,6 @@ export default function Controller() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Landing/>}></Route>
-          <Route path='/how/it/works' element={<HowItWorks/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
