f499ade8ec6b732bd0c5e3ce9dd0fe4e047f1865
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-05 18:45:38 +0530

    chore: enhance Landing component with logo and adjust layout for better visual appeal

--- mern-front/src/pages/Landing.tsx
+++ mern-front/src/pages/Landing.tsx
@@ -1,8 +1,13 @@
+import logo from '../assets/logo-icon.png'
+
 export default function Landing() {
     return (
         <div>
             <nav className='flex justify-between items-center w-full px-20 mt-4'>
-                <h1 className='text-lg font-semibold'>stability.ai</h1>
+                <div className='flex items-center justify-center gap-2'>
+                    <img src={logo} alt="" className='h-8'/>
+                    <h1 className='text-lg font-semibold'>stability.ai</h1>
+                </div>
                 <div className='flex items-center justify-center gap-4'>
                     <a className='font-semibold text-sm' href="">Home</a>
                     <a className='font-semibold text-sm' href="">Features</a>
@@ -19,6 +24,7 @@ export default function Landing() {
                 <p className='text-neutral-600 text-center'>Turn your ideas into high-quality, visually striking images
                     with powerful AI.
                     <br/> Simply type your prompt, pick a style, and watch your imagination come to life—instantly.</p>
+
                 <div className='flex justify-center items-center gap-3'>
                     <button className='font-semibold text-white bg-black px-10 py-2.5 rounded-md cursor-pointer hover:opacity-80'>Get
                         Started
@@ -27,38 +33,36 @@ export default function Landing() {
                         Create
                     </button>
                 </div>
-                <div className='flex justify-center items-center flex-col gap-8 mt-10 py-15 w-full '>
-                    <div className='flex justify-center items-center gap-25'>
-                        <div className='flex flex-col justify-center items-center gap-2'>
-                            <p className='text-4xl font-semibold'>12 million</p>
-                            <p className='text-sm text-neutral-600 font-semibold'>AI Images Generated</p>
-                        </div>
-                        <div className='flex flex-col justify-center items-center gap-2'>
-                            <p className='text-4xl font-semibold'>4.8 million</p>
-                            <p className='text-sm text-neutral-600 font-semibold'>Active Creators Worldwide</p>
-                        </div>
-                        <div className='flex flex-col justify-center items-center gap-2'>
-                            <p className='text-4xl font-semibold'>96 million</p>
-                            <p className='text-sm text-neutral-600 font-semibold'>Prompts Processed</p>
-                        </div>
+            </div>
+            <div className='flex justify-center items-center flex-col gap-8 mt-20'>
+                <div className='flex justify-center items-center gap-25'>
+                    <div className='flex flex-col justify-center items-center gap-2'>
+                        <p className='text-4xl font-semibold'>12 million</p>
+                        <p className='text-sm text-neutral-600 font-semibold'>AI Images Generated</p>
                     </div>
+                    <div className='flex flex-col justify-center items-center gap-2'>
+                        <p className='text-4xl font-semibold'>4.8 million</p>
+                        <p className='text-sm text-neutral-600 font-semibold'>Active Creators Worldwide</p>
+                    </div>
+                    <div className='flex flex-col justify-center items-center gap-2'>
+                        <p className='text-4xl font-semibold'>96 million</p>
+                        <p className='text-sm text-neutral-600 font-semibold'>Prompts Processed</p>
+                    </div>
+                </div>
 
-                    <div className='flex justify-center items-center gap-25'>
-                        <div className='flex flex-col justify-center items-center gap-2'>
-                            <p className='text-4xl font-semibold'>320 000</p>
-                            <p className='text-sm text-neutral-600 font-semibold'>New Users Every Month</p>
-                        </div>
-                        <div className='flex flex-col justify-center items-center gap-2'>
-                            <p className='text-4xl font-semibold'>8500+</p>
-                            <p className='text-sm text-neutral-600 font-semibold'>Custom Styles & Models Available</p>
-                        </div>
-                        <div className='flex flex-col justify-center items-center gap-2'>
-                            <p className='text-4xl font-semibold'>99.9%</p>
-                            <p className='text-sm text-neutral-600 font-semibold'>Uptime & System Reliability</p>
-                        </div>
+                <div className='flex justify-center items-center gap-25'>
+                    <div className='flex flex-col justify-center items-center gap-2'>
+                        <p className='text-4xl font-semibold'>320 000</p>
+                        <p className='text-sm text-neutral-600 font-semibold'>New Users Every Month</p>
                     </div>
-                    <p className='text-center text-neutral-600 mb-5'>Our AI platform empowers millions of creators worldwide.
-                        Generate high-quality images instantly, explore thousands <br/> of unique styles, and bring your imagination to life—effortlessly and reliably.</p>
+                    <div className='flex flex-col justify-center items-center gap-2'>
+                        <p className='text-4xl font-semibold'>8500+</p>
+                        <p className='text-sm text-neutral-600 font-semibold'>Custom Styles & Models Available</p>
+                    </div>
+                    <div className='flex flex-col justify-center items-center gap-2'>
+                        <p className='text-4xl font-semibold'>99.9%</p>
+                        <p className='text-sm text-neutral-600 font-semibold'>Uptime & System Reliability</p>
+                    </div>
                 </div>
             </div>
         </div>
