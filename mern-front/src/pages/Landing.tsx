21763f1ae0f6930aaf8b0db5541eb96a48528edb
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-05 18:38:42 +0530

    chore: update title and enhance Landing component layout and content

--- mern-front/src/pages/Landing.tsx
+++ mern-front/src/pages/Landing.tsx
@@ -4,28 +4,61 @@ export default function Landing() {
             <nav className='flex justify-between items-center w-full px-20 mt-4'>
                 <h1 className='text-lg font-semibold'>stability.ai</h1>
                 <div className='flex items-center justify-center gap-4'>
-                    <a href="">Home</a>
-                    <a href="">Features</a>
-                    <a href="">Models</a>
-                    <a href="">Legal & License</a>
-                    <a href="">Company</a>
-                    <a href="">Deployment</a>
-                    <a href="">Applications</a>
+                    <a className='font-semibold text-sm' href="">Home</a>
+                    <a className='font-semibold text-sm' href="">Features</a>
+                    <a className='font-semibold text-sm' href="">Models</a>
+                    <a className='font-semibold text-sm' href="">Legal & License</a>
+                    <a className='font-semibold text-sm' href="">Company</a>
+                    <a className='font-semibold text-sm' href="">Deployment</a>
+                    <a className='font-semibold text-sm' href="">Applications</a>
                 </div>
             </nav>
 
-            <div className='flex flex-col justify-center items-center gap-5 mt-20'>
+            <div className='flex flex-col justify-center items-center gap-8 mt-32'>
                 <h1 className='text-5xl font-semibold text-center leading-14'>Create Stunning AI Images Instantly With <br/> the Power of Smart Imagination.</h1>
                 <p className='text-neutral-600 text-center'>Turn your ideas into high-quality, visually striking images
                     with powerful AI.
                     <br/> Simply type your prompt, pick a style, and watch your imagination come to life—instantly.</p>
                 <div className='flex justify-center items-center gap-3'>
-                    <button className='font-semibold text-white bg-black px-10 py-2.5 rounded-md cursor-pointer'>Get
+                    <button className='font-semibold text-white bg-black px-10 py-2.5 rounded-md cursor-pointer hover:opacity-80'>Get
                         Started
                     </button>
                     <button className='font-semibold px-10 py-2.5 rounded-md cursor-pointer border border-black'>Start
                         Create
                     </button>
+                </div>
+                <div className='flex justify-center items-center flex-col gap-8 mt-10 py-15 w-full '>
+                    <div className='flex justify-center items-center gap-25'>
+                        <div className='flex flex-col justify-center items-center gap-2'>
+                            <p className='text-4xl font-semibold'>12 million</p>
+                            <p className='text-sm text-neutral-600 font-semibold'>AI Images Generated</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center gap-2'>
+                            <p className='text-4xl font-semibold'>4.8 million</p>
+                            <p className='text-sm text-neutral-600 font-semibold'>Active Creators Worldwide</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center gap-2'>
+                            <p className='text-4xl font-semibold'>96 million</p>
+                            <p className='text-sm text-neutral-600 font-semibold'>Prompts Processed</p>
+                        </div>
+                    </div>
+
+                    <div className='flex justify-center items-center gap-25'>
+                        <div className='flex flex-col justify-center items-center gap-2'>
+                            <p className='text-4xl font-semibold'>320 000</p>
+                            <p className='text-sm text-neutral-600 font-semibold'>New Users Every Month</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center gap-2'>
+                            <p className='text-4xl font-semibold'>8500+</p>
+                            <p className='text-sm text-neutral-600 font-semibold'>Custom Styles & Models Available</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center gap-2'>
+                            <p className='text-4xl font-semibold'>99.9%</p>
+                            <p className='text-sm text-neutral-600 font-semibold'>Uptime & System Reliability</p>
+                        </div>
+                    </div>
+                    <p className='text-center text-neutral-600 mb-5'>Our AI platform empowers millions of creators worldwide.
+                        Generate high-quality images instantly, explore thousands <br/> of unique styles, and bring your imagination to life—effortlessly and reliably.</p>
                 </div>
             </div>
         </div>
