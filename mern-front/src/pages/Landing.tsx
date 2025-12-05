69ee9b16e14805fa857726e5fb2a3080b8852b09
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-05 17:27:55 +0530

    chore: update title and enhance Landing component layout and content

--- mern-front/src/pages/Landing.tsx
+++ mern-front/src/pages/Landing.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
 
 export default function Landing() {
   return (
     <div>
-      
+      <nav className='flex justify-between items-center w-full px-20 mt-4'>
+          <h1 className='text-lg font-semibold'>imagine.ai</h1>
+      </nav>
+
+        <div className='flex flex-col justify-center items-center'>
+            <h1 className='text-6xl font-semibold text-center'>Create Stunning AI Images<br/> in Seconds</h1>
+            <p className='text-neutral-400 text-center mt-3'>Turn your ideas into high-quality, visually striking images with powerful AI.
+               <br/> Simply type your prompt, pick a style, and watch your imagination come to life—instantly.</p>
+            <button></button>
+        </div>
     </div>
   )
 }
