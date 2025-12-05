78e57d2796b412429f2438bcc61f55690716c029
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-05 17:35:42 +0530

    chore: enhance Landing component layout and update button styles

--- mern-front/src/pages/Landing.tsx
+++ mern-front/src/pages/Landing.tsx
@@ -7,10 +7,13 @@ export default function Landing() {
       </nav>
 
         <div className='flex flex-col justify-center items-center'>
-            <h1 className='text-6xl font-semibold text-center'>Create Stunning AI Images<br/> in Seconds</h1>
-            <p className='text-neutral-400 text-center mt-3'>Turn your ideas into high-quality, visually striking images with powerful AI.
+            <h1 className='text-6xl font-semibold text-center tracking-tighter'>Create Stunning AI Images<br/> in Seconds.</h1>
+            <p className='text-neutral-500 text-center mt-3'>Turn your ideas into high-quality, visually striking images with powerful AI.
                <br/> Simply type your prompt, pick a style, and watch your imagination come to life—instantly.</p>
-            <button></button>
+            <div className='flex justify-center items-center gap-3 mt-4'>
+                <button className='font-semibold text-white bg-black px-10 py-2.5 rounded-md cursor-pointer'>Get Started</button>
+                <button className='font-semibold px-10 py-2.5 rounded-md cursor-pointer border border-black'>Start Create</button>
+            </div>
         </div>
     </div>
   )
