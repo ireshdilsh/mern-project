9d9fb7f580823d7defe3504c46e3f410675a713d
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-06 07:44:47 +0530

    chore: create LandingPage component and update routing in Controller

--- mern-front/src/Controller.tsx
+++ mern-front/src/Controller.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css' 
 import LandingPage from './pages/LandingPage'
+import Landing from "./pages/Landing.tsx";
 
 export default function Controller() {
   return (
@@ -8,6 +9,7 @@ export default function Controller() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<LandingPage/>}></Route>
+            <Route path='/home' element={<Landing/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
