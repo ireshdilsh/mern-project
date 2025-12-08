7a8aa642c77d761e4aa0cf80e2d607c474d2940c
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-08 12:13:00 +0530

    feat: implement AI image generation controller with error handling

--- src/controller/ai.generate.controller.ts
+++ src/controller/ai.generate.controller.ts
@@ -1,22 +1,7 @@
-import Bytez from "bytez.js"
-
-export const gnerateAiImage = async (req: any, res: any) => {
-
-    const prompt = req.body.prompt;
-
-    try {
-        const key = "f669d32c7b681ebb403eaf797932f835"
-        const sdk = new Bytez(key)
-
-        // choose stable-diffusion-xl-base-1.0
-        const model = sdk.model("stabilityai/stable-diffusion-xl-base-1.0")
-
-        // send input to model
-        const { error, output } = await model.run({prompt: prompt})
-
-        console.log({ error, output });
-    } catch (error) {
-        console.error("Error generating image:", error);
-        res.status(500).json({ message: "Error generating image", error });
+export const generateAiImage = (req:any,res:any) => {
+    try{
+        
+    }catch(error){
+        res.status(500).json({message: "Internal Server Error ",error});   
     }
 }
--- src/routes/ai.route.ts
+++ src/routes/ai.route.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import { gnerateAiImage } from "../controller/ai.generate.controller.js";
+import { generateAiImage } from "../controller/ai.generate.controller.js";
 
 const router = express.Router();
 
-router.post("/ai",gnerateAiImage)
+router.post("/ai",generateAiImage)
 
 export default router;
