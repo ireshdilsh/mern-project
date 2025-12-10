cdc5a8338f6a46d2d70f7f7eed2da86441cf15a0
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-10 21:18:16 +0530

    feat: add cloudinary configuration and multer upload middleware

--- src/controller/ai.generate.controller.ts
+++
@@ -1,7 +0,0 @@
-export const generateAiImage = (req:any,res:any) => {
-    try{
-        
-    }catch(error){
-        res.status(500).json({message: "Internal Server Error ",error});   
-    }
-}
--- src/routes/ai.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { generateAiImage } from "../controller/ai.generate.controller.js";
-
-const router = express.Router();
-
-router.post("/ai",generateAiImage)
-
-export default router;
