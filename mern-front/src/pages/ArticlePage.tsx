a2871af4f7f8846f755429304b3f7fdceaf9b77b
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-10 10:45:20 +0530

    fix: remove unused imports from ArticlePage component

--- mern-front/src/pages/ArticlePage.tsx
+++ mern-front/src/pages/ArticlePage.tsx
@@ -8,8 +8,8 @@ export default function ArticlePage() {
       <div className='flex flex-col justify-center items-center px-40 mt-15 gap-7'>
         <input type="text" placeholder='Title' className='text-5xl w-200 outline-none text-[#333]' id='write-title' />
         <div className='flex gap-4'>
-          <button className='bg-amber-300'>
-            <img src="https://img.icons8.com/?size=100&id=24717&format=png&color=999999" className='h-9 cursor-pointer' alt="" />
+          <button className='h-8'>
+            <img src="https://img.icons8.com/?size=100&id=24717&format=png&color=999999" className='h-8 cursor-pointer' alt="" />
           </button>
           <textarea name="" id="" className='text-2xl w-215 h-90 outline-none' placeholder='Tell your story ....'></textarea>
         </div>
