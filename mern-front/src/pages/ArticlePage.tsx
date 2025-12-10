a72f320ae70b2ab7870d5e49911086461c8d0721
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-10 11:28:33 +0530

    fix: integrate UserNavbar component into ArticlePage

--- mern-front/src/pages/ArticlePage.tsx
+++ mern-front/src/pages/ArticlePage.tsx
@@ -5,6 +5,10 @@ export default function ArticlePage() {
 
   const [imagePreview, setImagePreview] = useState<string | undefined>();
 
+  const [title, setTitle] = useState<string>('');
+  const [content, setContent] = useState<string>('');
+  const [image, setImage] = useState<File | null>(null);
+
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
@@ -16,11 +20,19 @@ export default function ArticlePage() {
     }
   }
 
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  }
+
+  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  }
+
   return (
     <div>
       <UserNavbar />
       <div className='flex flex-col justify-center items-center px-40 mt-15 gap-7'>
-        <input type="text" placeholder='Title' className='text-5xl w-200 outline-none text-[#333]' id='write-title' />
+        <input onChange={handleTitleChange} type="text" placeholder='Title' className='text-5xl w-200 outline-none text-[#333]' id='write-title' />
         <img src={imagePreview} className='' alt="" />
         <div className='flex gap-4'>
           <button className='h-8'>
@@ -29,7 +41,7 @@ export default function ArticlePage() {
             </label>
           </button>
           <input type="file" id="file-input" onChange={handleImageChange} style={{ display: 'none' }} />
-          <textarea name="" id="" className='write-content text-2xl w-212 h-90 outline-none' placeholder='Tell your story ....'></textarea>
+          <textarea onChange={handleContentChange} name="" id="" className='write-content text-2xl w-212 h-90 outline-none' placeholder='Tell your story ....'></textarea>
         </div>
         <button className='bg-green-700 text-white font-medium text-sm px-2.5 py-1 rounded-4xl cursor-pointer absolute right-54 top-5'>Publish</button>
       </div>
