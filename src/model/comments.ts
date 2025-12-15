f6116a159fa2709024ff92de1a68404d9bb1b540
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-15 14:20:30 +0530

    fix: update comment schema to ensure required fields are validated

--- src/model/comments.ts
+++ src/model/comments.ts
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
+interface IComment extends mongoose.Document {
+    _id: mongoose.Types.ObjectId,
+    article_id: mongoose.Types.ObjectId,
+    name: String,
+    content: String,
+    createdAt: Date
+}
+
 const commentSchema = new mongoose.Schema({
      article_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Article', required: true },
      name: { type: String, required: true },
@@ -7,4 +15,4 @@ const commentSchema = new mongoose.Schema({
      createdAt: { type: Date, default: Date.now }
 })
 
-export const Comment = mongoose.model("Comment", commentSchema);
+export const Comment = mongoose.model<IComment>("Comment", commentSchema);
