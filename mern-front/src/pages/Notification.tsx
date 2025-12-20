2cda113264616e6be515c0db2be3dd2792da4d81
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-20 19:24:06 +0530

    update desin

--- mern-front/src/pages/Notification.tsx
+++ mern-front/src/pages/Notification.tsx
@@ -5,6 +5,7 @@ export default function Notification() {
   return (
     <div>
       <UserNavbar/>
+      <p className='text-3xl tracking-tighter mt-10 mx-25'>Notifications ({23})</p>
     </div>
   )
 }
