2c769d2ddd435a10ab632e56d7f0d7f7c1a2967a
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-23 09:39:32 +0530

    fix: improve formatting and add missing newlines in user.controller.ts and .env files

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,12 +1,26 @@
 import { User } from "../model/user.js";
 
-export const saveUser = async(res:any,req:any) => {
+export const saveUser = async (res: any, req: any) => {
     try {
-        const {name,email,password} = req.body;
-        const newUser = new User({name,email,password});
+        const { name, email, password } = req.body;
+        const newUser = new User({ name, email, password });
         await newUser.save();
-        return res.status(201).send({message:"User saved successfully",newUser})
-    }catch (e) {
-        return res.status(400).send({message:"Error saving user",error:e})
+        return res.status(201).send({ message: "User saved successfully", newUser })
+    } catch (e) {
+        return res.status(400).send({ message: "Error saving user", error: e })
+    }
+}
+
+
+export const loginUser = async (res: any, req: any) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email, password });
+        if (user) {
+            return res.status(200).send({ message: "User logged in successfully", user })
+        }
+        return null
+    } catch (error) {
+        return res.status(400).send({ message: "Error logging in user", error: error })
     }
 }
