af7713934a737faf322ed3a5e6cefde15b3afa4c
ireshdilsh <ireshdilshan46@gmail.com>
2025-12-26 22:44:37 +0530

    fix: improve formatting by adding spaces after commas in article routes

--- src/routes/article.route.ts
+++ src/routes/article.route.ts
@@ -11,10 +11,10 @@ import { upload } from "../middleware/upload.js";
 
 export const article_router = express.Router();
 
-article_router.post('/create/article',saveArticle)
-article_router.get('/get/all/articles',getAllSavedArticles)
-article_router.get('/get/article/by/:id',getArticleById)
+article_router.post('/create/article', saveArticle)
+article_router.get('/get/all/articles', getAllSavedArticles)
+article_router.get('/get/article/by/:id', getArticleById)
 article_router.post('/create/article/with/image', upload.single('file'), saveArticleWithImage)
-article_router.get('/find/article/:search',searchArticle)
-article_router.delete('/delete/article/:id',deleteArticleByID)
-article_router.get('/get/articles/with/:email',getArticlesByAuthorEmail)
+article_router.get('/find/article/:search', searchArticle)
+article_router.delete('/delete/article/:id', deleteArticleByID)
+article_router.get('/get/articles/with/:email', getArticlesByAuthorEmail)
